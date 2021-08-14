cc068c2fb216fac07d3c14fbdbaef579d1a697b2
DESKTOP-8RDTF2G\Taller <tallerGitDLC@gmail.com>
2021-08-14 10:12:14 +0200

    update: division added to script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int a = 6 / 2;
     }
 
     // Update is called once per frame
